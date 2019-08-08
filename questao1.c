a998fe540af8da3811332a6384c446018b9ed150
XSrDanDanX <ddipaumirim@gmail.com>
2019-08-08 18:07:11 -0300

    Torre de hanoi quase concluida!

--- questao1.c
+++ questao1.c
@@ -6,7 +6,7 @@
 
 void preencherPino(Pilha *p){
 	int i;
-	for(i = 1 ; i <= TAMANHO_MAX; i++){
+	for(i = TAMANHO_MAX ; i >= 1; i--){
 		push(p, i);
 	}
 }
@@ -14,7 +14,7 @@ void preencherPino(Pilha *p){
 
 int retirarDisco(Pilha *pino){
 	if(isEmptyPilha(pino)){
-		printf("\nPino vazio!");
+		printf("\nPino vazio!\n");
 		return 0;
 	}else{
 		return pop(pino);
@@ -22,33 +22,48 @@ int retirarDisco(Pilha *pino){
 }
 
 
-void inserirDisco(Pilha *pino, int disco){
-	if(disco > top(pino)){
-		printf("\nMovimento invalido!");
-	}else{
+int inserirDisco(Pilha *pino, int disco){
+	
+	if(isEmptyPilha(pino)){
 		push(pino, disco);
+		return 1;
+	}else{
+		
+		if(disco > top(pino)){
+			printf("\nMovimento invalido!\n");
+			return 0;
+		}else{
+			push(pino, disco);
+			return 1;
+		}
+
 	}
+	
+	
 }
 
 void imprimirPino(Pilha *pino){
-	
 	Pilha *aux = new_Pilha();
-	printf("|");
 	while(!isEmptyPilha(pino)){
 		push(aux, pop(pino));
-		printf(" %d |",top(aux));
 	}
-	printf("\n");
 	
+	printf("|");
 	while(!isEmptyPilha(aux)){
 		push(pino, pop(aux));
+		printf(" %d |",top(pino));
 	}
-	
+	printf("\n");
 }
 
-void menu(){
-	printf("\n============= TORRE DE HANOI ==============\n");
-	
+void menu(Pilha *esq, Pilha *cen, Pilha *dir){
+	printf("================== TORRE DE HANOI =====================\n");
+		printf("Pino Esquerdo (1): ");
+		imprimirPino(esq);
+		printf("Pino Central (2): ");
+		imprimirPino(cen);
+		printf("Pino Direito (3): ");
+		imprimirPino(dir);		
 }
 
 
@@ -58,9 +73,115 @@ void torreHanoi(){
 	Pilha *dir = new_Pilha();
 	
 	preencherPino(esq);
+	int opcao;
+	int valor;
+	while(sizePilha(dir) < TAMANHO_MAX){
+		system("cls");
+		menu(esq, cen, dir);		
+		printf("Informe em qual pino quer remover: ");
+		scanf("%d",&opcao);
+		switch(opcao){
+			case 1:
+				
+				system("cls");
+				valor = retirarDisco(esq);		
+				menu(esq, cen, dir);
+				printf("Informe em qual pino quer adicionar: ");
+				scanf("%d",&opcao);
+				switch(opcao){
+					case 1:
+							
+						if(!inserirDisco(esq, valor)){
+							inserirDisco(esq, valor);	
+						}
+						
+						break;
+					case 2:
+							
+						if(!inserirDisco(cen, valor)){
+							inserirDisco(esq, valor);	
+						}
+						break;
+					case 3:
+							
+						if(!inserirDisco(dir, valor)){
+							inserirDisco(esq, valor);	
+						}
+						break;
+					default:
+						break;
+				}
+				break;
+			case 2:
+				system("cls");
+				
+				valor = retirarDisco(cen);
+				menu(esq, cen, dir);
+				printf("Informe em qual pino quer adicionar: ");
+				scanf("%d",&opcao);
+				switch(opcao){
+					case 1:
+						
+						if(!inserirDisco(esq, valor)){
+							inserirDisco(cen, valor);	
+						}
+						break;
+					case 2:
+						
+							
+						if(!inserirDisco(cen, valor)){
+							inserirDisco(cen, valor);	
+						}
+						break;
+						
+					case 3:
+							
+						if(!inserirDisco(dir, valor)){
+							inserirDisco(cen, valor);	
+						}
+						break;
+					default:
+						break;
+				}
+				break;
+			case 3:
+				
+				system("cls");
+				valor = retirarDisco(dir);
+				menu(esq, cen, dir);
+				printf("Informe em qual pino quer adicionar: ");
+				scanf("%d",&opcao);
+				switch(opcao){
+					case 1:
+							
+						if(!inserirDisco(esq, valor)){
+							inserirDisco(dir, valor);	
+						}
+						break;
+					case 2:
+						
+						if(!inserirDisco(cen, valor)){
+							inserirDisco(dir, valor);	
+						}
+						break;
+					case 3:
+						
+							
+						if(!inserirDisco(dir, valor)){
+							inserirDisco(dir, valor);	
+						}
+						break;
+					default:
+						break;
+				}
+				break;
+			default:
+				break;	
+		}
+		
+		
+	}
 	
-	imprimirPino(esq);
-	imprimirPino(cen);
-	imprimirPino(dir);
-	imprimirPino(esq);		
+	
+	
 }
